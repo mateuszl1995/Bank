593714d6e2ff819d16016d174522edcdde5c43cc
Nuras998 <nuras9988@gmail.com>
2018-03-09 09:33:23 +0100

    Create classes.

---
+++ src/Bank.java
@@ -0,0 +1,9 @@
+public class Bank {
+    History history;
+
+    public void execute(BankOperation bankOperation) {
+        if (bankOperation.execute()) {
+            history.save(bankOperation);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Bank bank = new Bank();
+
+        bank.execute(new BankOperation(new BankProduct(123)));
     }
 }
