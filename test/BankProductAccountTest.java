3e5a46907242a776daeb5a76ef400f55441b90a4
Mateusz Lewandowski <mateusz.lewandowski@onet.eu>
2018-04-06 02:18:42 +0200

    Dodanie do przykładu mockito

--- test/BankProductAccountTest.java
+++ test/BankProductAccountTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class BankProductAccountTest {
     BankProductAccount account;
@@ -8,10 +11,15 @@ public class BankProductAccountTest {
     Client client;
 
     @Test
-    public void testConstructor() {
-        interest = new InterestZero();
+    public void testFirstForExcerciseJunitAndMockito() {
+        interest = mock(InterestZero.class);
+        when(interest.calculateInterest(account)).thenReturn(30.0f);
         account = new BankProductAccount(103, interest);
         assertNotNull(account);
+        assertEquals(2, 2);
+        // assertTrue(2 > 2);
+        float x = interest.calculateInterest(account);
+        assertEquals(x, 29.0f);
     }
 
 
